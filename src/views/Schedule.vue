8f155821d9fff4e65057e68390b587856fb0880c
eche24 <spwesn@gmail.com>
2021-01-04 20:27:56 +0100

    add schedule

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-black flex-1 ml-56 px-10 text-white pt-10">
+  <div class="bg-black flex-1 ml-56 px-10 text-white pt-10" style="font-family: 'Poppins', sans-serif;">
     <h1 class="mb-4">Schedule Post</h1>
 
     <div class="nav-schedule">
